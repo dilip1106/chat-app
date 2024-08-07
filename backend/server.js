8b6f88e594754822f7b91aedb7bb3c00cd1627dd
dilip1106 <dilipteli38@gmail.com>
2024-08-07 23:55:19 +0530

    final deployment

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import authRoutes from "./routes/auth.routes.js";
 import messagesRoutes from "./routes/messages.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
-import connectToMongoDB from "./db/connecttoMongoDB.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
 dotenv.config();
